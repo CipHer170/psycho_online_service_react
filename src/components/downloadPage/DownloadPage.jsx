fba68f41f63f8ce82d5360cb0d80d03abfdd6c4c
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-01-24 15:01:50 +0500

    buttons google apps

--- src/components/downloadPage/DownloadPage.jsx
+++ src/components/downloadPage/DownloadPage.jsx
@@ -16,15 +16,39 @@ function DownloadPage() {
             Point — это современное приложение, в котором вы можете получить
             психологическую поддержку, когда и где вам удобно.
           </span>
-          <span className="downloadPage__title-secondary"></span>
+          <span className="downloadPage__title-secondary">
+            Download our app
+          </span>
+          <span className="downloadPage__page">
+            Скачайте приложение на ваш смартфон и пройдите быструю регистрацию.
+          </span>
         </div>
         <div className="downloadPage__buttons">
-          <button className="downloadPage__btn btn__android">
+          {/* <button className="downloadPage__btn btn__android">
             Выбрать специалиста
           </button>
           <button className="downloadPage__btn btn__ios">
             Попробовать бесплатно
-          </button>
+          </button> */}
+          <a
+            href="https://www.kobinet.com.tr/"
+            target="_blank"
+            class="market-btn apple-btn"
+            role="button"
+          >
+            <span class="market-button-subtitle">Download on the</span>
+            <span class="market-button-title">App Store</span>
+          </a>
+
+          <a
+            href="https://www.kobinet.com.tr/"
+            target="_blank"
+            class="market-btn google-btn"
+            role="button"
+          >
+            <span class="market-button-subtitle">Download on the</span>
+            <span class="market-button-title">Google Play</span>
+          </a>
         </div>
       </div>
 
